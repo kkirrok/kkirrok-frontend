f2f77f13947e1b6e86eb0ea289537a902c2b0972
chae1125 <y72682685@gmail.com>
2026-05-26 01:09:59 +0900

    ♿chore: 알림 버튼 접근성 속성 및 hitSlop 추가

--- components/KkLogoHeader.tsx
+++ components/KkLogoHeader.tsx
@@ -12,7 +12,13 @@ export default function KkLogoHeader() {
         {/* 임시 로고 이미지 */}
         <BellIcon width={24} height={24} />
 
-        <TouchableOpacity onPress={() => router.push("/notification")}>
+        <TouchableOpacity
+          onPress={() => router.push("/notification")}
+          accessibilityRole="button"
+          accessibilityLabel="알림 화면으로 이동"
+          accessibilityHint="알림 목록 화면을 엽니다"
+          hitSlop={{ top: 8, bottom: 8, left: 8, right: 8 }}
+        >
           <BellIcon width={24} height={24} />
         </TouchableOpacity>
       </View>
