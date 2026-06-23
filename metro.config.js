670fd3f18ada91a1a1312f99c142123dca21ce59
chae1125 <y72682685@gmail.com>
2026-06-23 23:33:03 +0900

    🔧 fix: 소셜 로그인 패키지 설치 및 빌드 설정

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
@@ -7,10 +8,25 @@ config.transformer = {
   babelTransformerPath: require.resolve('react-native-svg-transformer'),
 };
 
+const kakaoPackages = [
+  '@react-native-kakao/user',
+  '@react-native-kakao/core',
+];
+
 config.resolver = {
   ...config.resolver,
   assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
   sourceExts: [...config.resolver.sourceExts, 'svg'],
+  resolveRequest: (context, moduleName, platform) => {
+    if (kakaoPackages.includes(moduleName)) {
+      const pkg = require(path.join(__dirname, 'node_modules', moduleName, 'package.json'));
+      return {
+        filePath: path.join(__dirname, 'node_modules', moduleName, `${pkg.main}.js`),
+        type: 'sourceFile',
+      };
+    }
+    return context.resolveRequest(context, moduleName, platform);
+  },
 };
 
 module.exports = config;
